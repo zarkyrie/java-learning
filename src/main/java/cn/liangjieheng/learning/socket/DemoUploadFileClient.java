ee0baebaf61bfd890e83e97ff17e81d4ee3cbad4
JH L <ljh01124578@163.com>
2018-04-26 13:54:50 +0800

    file upload demo

---
+++ src/main/java/cn/liangjieheng/learning/socket/DemoUploadFileClient.java
@@ -0,0 +1,57 @@
+package cn.liangjieheng.learning.socket;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+public class DemoUploadFileClient extends Socket {
+
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 12018;
+
+    private Socket client;
+    private FileInputStream fis;
+    private DataOutputStream dos;
+
+    public DemoUploadFileClient() {
+        try {
+            try {
+                client = new Socket(SERVER_IP, SERVER_PORT);
+                //向服务端传送文件
+                File file = new File("c:/test.doc");
+                fis = new FileInputStream(file);
+                dos = new DataOutputStream(client.getOutputStream());
+
+                //文件名和长度
+                dos.writeUTF(file.getName());
+                dos.flush();
+                dos.writeLong(file.length());
+                dos.flush();
+
+                //传输文件
+                byte[] sendBytes = new byte[1024];
+                int length = 0;
+                while ((length = fis.read(sendBytes, 0, sendBytes.length)) > 0) {
+                    dos.write(sendBytes, 0, length);
+                    dos.flush();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (fis != null)
+                    fis.close();
+                if (dos != null)
+                    dos.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new DemoUploadFileClient();
+    }
+
+}
