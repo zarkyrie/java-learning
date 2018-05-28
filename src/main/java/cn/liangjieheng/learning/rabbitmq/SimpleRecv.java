bb0bb5d4751f74a178798655836f5cb523eda216
JH L <ljh01124578@163.com>
2018-05-28 22:20:49 +0800

    rabbitmq demo

---
+++ src/main/java/cn/liangjieheng/learning/rabbitmq/SimpleRecv.java
@@ -0,0 +1,33 @@
+package cn.liangjieheng.learning.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class SimpleRecv {
+
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.56.101");
+        connectionFactory.setUsername("ljh");
+        connectionFactory.setPassword("ljh");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
---
+++ src/main/java/cn/liangjieheng/learning/rabbitmq/SimpleSend.java
@@ -0,0 +1,29 @@
+package cn.liangjieheng.learning.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class SimpleSend {
+
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.56.101");
+        connectionFactory.setUsername("ljh");
+        connectionFactory.setPassword("ljh");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
