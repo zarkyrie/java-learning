6d2b1a01994166a2a4101cddbba3e3a59d951792
JH L <ljh01124578@163.com>
2018-06-22 17:45:35 +0800

    生产者消费者实现

---
+++ src/main/java/cn/liangjieheng/learning/concurrent/ConsumerProducer.java
@@ -0,0 +1,60 @@
+package cn.liangjieheng.learning.concurrent;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ConsumerProducer {
+
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(3);
+
+        Thread t1 = new Thread(new Producer(arrayBlockingQueue));
+        t1.start();
+        Thread t2 = new Thread(new Consumer(arrayBlockingQueue));
+        t2.start();
+    }
+
+    static class Producer implements Runnable {
+
+        ArrayBlockingQueue<String> arrayBlockingQueue;
+
+        public Producer(ArrayBlockingQueue<String> arrayBlockingQueue) {
+            this.arrayBlockingQueue = arrayBlockingQueue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    arrayBlockingQueue.put("message:" + i);
+                }
+                arrayBlockingQueue.put("byebye");
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+
+        }
+    }
+
+    static class Consumer implements Runnable {
+
+        ArrayBlockingQueue<String> arrayBlockingQueue;
+
+        public Consumer(ArrayBlockingQueue<String> arrayBlockingQueue) {
+            this.arrayBlockingQueue = arrayBlockingQueue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String msg;
+                while (!"byebye".equalsIgnoreCase(msg = arrayBlockingQueue.take())) {
+                    System.out.println(msg);
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+}
