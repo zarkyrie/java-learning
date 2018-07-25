edd006dd31c4d96fc64947367ee132012423d93a
JH L <ljh01124578@163.com>
2018-07-25 17:41:53 +0800

    快速排序算法

---
+++ src/main/java/cn/liangjieheng/learning/Test.java
@@ -0,0 +1,52 @@
+package cn.liangjieheng.learning;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Test {
+
+    static abstract class Human{
+
+    }
+
+    static class Man extends Human{
+
+    }
+
+    static class Women extends Human{
+
+    }
+
+    public void show(Human human){
+        System.out.println("hello human");
+    }
+
+    public void show(Man man){
+        System.out.println("hello man");
+    }
+
+    public void show(Women women){
+        System.out.println("hello women");
+    }
+
+    public static void main(String[] args) {
+//        Human man = new Man();
+//        Human women = new Women();
+//
+//        Test t = new Test();
+//        t.show(man);
+//        t.show(women);
+
+        Map<String,String> map = new HashMap<>();
+        map.put("11","1");
+        System.out.println(map.toString());
+
+    }
+
+
+}
+
+
+
+
---
+++ src/main/java/cn/liangjieheng/learning/algorithm/sort/QuickSortSample.java
@@ -0,0 +1,38 @@
+package cn.liangjieheng.learning.algorithm.sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QuickSortSample {
+
+    public static List<Integer> quickSort(List<Integer> list) {
+        if (list.size() < 2) {
+            return list;
+        } else {
+            Integer pivot = list.get(0);
+            List<Integer> less = new ArrayList<>();
+            List<Integer> greater = new ArrayList<>();
+            for (Integer i : list) {
+                if (i < pivot) {
+                    less.add(i);
+                } else if (i > pivot) {
+                    greater.add(i);
+                }
+            }
+            List<Integer> results = new ArrayList<>();
+            results.addAll(quickSort(less));
+            results.add(pivot);
+            results.addAll(quickSort(greater));
+            return results;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>(Arrays.asList(3, 1, 5, 9, 11, 7));
+        List<Integer> list = quickSort(l);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
+}
